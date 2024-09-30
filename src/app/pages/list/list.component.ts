9f117bb613ff78b651e3ac4d900b367c7243da1c
Andresoto <zuluandres27@gmail.com>
2024-09-30 14:15:10 -0500

    Refactor list component to remove unused code

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,10 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { selectTaskList } from 'src/app/store/task.selectors';
 import { PipesModule } from 'src/app/pipes/pipes.module';
 import { RouterLink } from '@angular/router';
-import { addTask, updateTaskStatus } from 'src/app/store/task.actions';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { TaskCardComponent } from 'src/app/components/task-card/task-card.component';
@@ -17,34 +16,11 @@ import { TaskCardComponent } from 'src/app/components/task-card/task-card.compon
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
 
   store = inject(Store);
 
   tasksList$ = this.store.select(selectTaskList);
   typeFilter: 'ALL' | 'PENDING' | 'DONE' = 'ALL';
 
-  ngOnInit(): void {
-    const task = {
-      "id": crypto.randomUUID(),
-      "name": "nueva tarea",
-      "date": "2024-09-19",
-      "status": "PENDING",
-      "person": [
-          {
-              "name": "Medina",
-              "age": 25,
-              "skills": [
-                  "Angular",
-                  "Typescript"
-              ]
-          }
-      ]
-    }
-    this.store.dispatch(
-      addTask({
-        task: task
-      })
-    )
-  }
 }
